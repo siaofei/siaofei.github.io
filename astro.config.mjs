8daa450a10aece93e9e0ad5f1915d8d6458542d5
Xiaofei Liu <xiaofei1356@gmail.com>
2024-08-16 13:31:04 +0800

    feat: add widget ScrollToTop
    - add unofficial astro-icon integration for svg
    - (astro-icon) temporary fix #231 in offical github repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import alpinejs from "@astrojs/alpinejs";
+import alpinejs from '@astrojs/alpinejs'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
-});
+  integrations: [tailwind(), alpinejs(), icon()],
+})
