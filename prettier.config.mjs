4be066060368511bcf55f431fd26821c3e163c49
Xiaofei Liu <xiaofei1356@gmail.com>
2024-08-16 10:27:05 +0800

    feat: install and config prettier
    - add astro support
    - add tailwind support
    - customize import sort order
    - add editorconfig file

---
+++ prettier.config.mjs
@@ -0,0 +1,44 @@
+/**
+ * Format you source code to consistent style.
+ *
+ * Native support: JavaScript, TypeScript, CSS, HTML, JSON, Markdown, YAML etc.
+ *
+ * Pluggin support: Astro
+ *
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  experimentalTernaries: true,
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss', // Must last
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  importOrder: [
+    '',
+    '<TYPES>^(node:)',
+    '<TYPES>',
+    '<TYPES>^[.]',
+    '',
+    '<BUILTIN_MODULES>', // Node.js built-in modules
+    '^(astro:)', // Astro built-in modules
+    '<THIRD_PARTY_MODULES>',
+    '^(@utils|@data)/(.*)$', // local modules
+    '',
+    '^(@component|@layouts)/(.*)$',
+    '^[./]',
+    '',
+  ],
+}
