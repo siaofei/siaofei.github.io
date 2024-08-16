a447c446836a81dff49a3980fc14b33bb03d4150
Xiaofei Liu <xiaofei1356@gmail.com>
2024-08-16 14:48:38 +0800

    feat: add Nav component

---
+++ src/data/menus.ts
@@ -0,0 +1,52 @@
+export type MenuType = {
+  name: string
+  uri: string
+}
+
+export const MenuAbout = {
+  name: '关于',
+  uri: '/about/',
+}
+
+export const MENUS: MenuType[] = [
+  {
+    name: '主页',
+    uri: '/',
+  },
+  MenuAbout,
+  {
+    name: '笔记',
+    uri: '/notes/',
+  },
+  {
+    name: '文章',
+    uri: '/blogs/',
+  },
+  {
+    name: '标签',
+    uri: '/tags/',
+  },
+  {
+    name: '归档',
+    uri: '/archives/',
+  },
+]
+
+export const HIDDEN_MENUS: MenuType[] = [
+  {
+    name: '工具',
+    uri: '/tools/',
+  },
+  {
+    name: '书签',
+    uri: '/bookmarks/',
+  },
+  {
+    name: '阅读',
+    uri: '/reading/',
+  },
+  {
+    name: '友链',
+    uri: '/friends/',
+  },
+]
