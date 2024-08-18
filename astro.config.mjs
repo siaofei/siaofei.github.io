db3328e186489b90434e04653cbd630a460854ee
Xiaofei Liu <xiaofei1356@gmail.com>
2024-08-18 17:32:46 +0800

    feat: add words and reading time for post
    - install reading-time and mdast-util-to-string
    - create remark-reading-time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
+import { remarkReadingTime } from './plugins/remark-reading-time.mjs'
+
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [remarkReadingTime],
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
