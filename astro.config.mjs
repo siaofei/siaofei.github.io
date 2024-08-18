b11e1e8a1e7f1a31f74b8c29eb8f8a585d7b3a55
Xiaofei Liu <xiaofei1356@gmail.com>
2024-08-18 17:52:20 +0800

    fix(config): fix remarkReadingTime configuration
    - Change remarkReadingTime to `markdown.remarkPlugins`.
    - Config shiki highlighter themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,13 @@ import { remarkReadingTime } from './plugins/remark-reading-time.mjs'
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    rehypePlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime],
+    shikiConfig: {
+      themes: {
+        light: 'catppuccin-latte',
+        dark: 'catppuccin-mocha',
+      },
+    },
   },
   integrations: [
     tailwind({
